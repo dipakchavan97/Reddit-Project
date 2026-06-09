ea4127a6584f9424e0ff9545fa2d0ba6a05068d6
Dipak K Chavan <dipakchavan.mail@gmail.com>
2026-06-09 14:56:20 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "tetris-bucket"
+    bucket         = "devsecopsproject97"
     region         = "us-east-1"
     key            = "EKS-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    use_lockfile   = true
     encrypt        = true
   }
   required_version = ">=0.13.0"
